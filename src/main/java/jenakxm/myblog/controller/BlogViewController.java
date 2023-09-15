2ca7acf87ef8620d3a1975484187e22fdb630077
jenakxm <jenakxm@gmail.com>
2023-09-15 22:20:17 +0900

    blog view with thymeleaf

---
+++ src/main/java/jenakxm/myblog/controller/BlogViewController.java
@@ -0,0 +1,38 @@
+package jenakxm.myblog.controller;
+
+import jenakxm.myblog.domain.Article;
+import jenakxm.myblog.dto.ArticleListViewResponse;
+import jenakxm.myblog.dto.ArticleViewResponse;
+import jenakxm.myblog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);   // 블로그 글 리스트 저장
+
+        return "articleList";   // articleList.html 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+}
